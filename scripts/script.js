19ee8ae08119aee2acc3270a28cf3c36fcac7e85
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-09-12 22:06:09 -0700

    Fixed formatting and started JS

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ let total = 0;
 let formTotalCost = document.getElementById("total-cost");
 
 // Function to add total dollars to check out. Figure out how to truncate the decimals to 2 places.
-function addToCheckOutTotal(){
+function addToCheckOutTotal() {
   total = total + Number(cost.value);
   checkoutTotal.textContent = "$" + total;
   alert("Your total is now: $" + total);
@@ -12,73 +12,73 @@ function addToCheckOutTotal(){
 
 
 
-function murphy(){
+function murphy() {
   let name = document.getElementsByTagName("h4")[0].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function poppy(){
+function poppy() {
   let name = document.getElementsByTagName("h4")[1].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function jack(){
+function jack() {
   let name = document.getElementsByTagName("h4")[2].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function duffy(){
+function duffy() {
   let name = document.getElementsByTagName("h4")[3].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function lucas(){
+function lucas() {
   let name = document.getElementsByTagName("h4")[4].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function jake(){
+function jake() {
   let name = document.getElementsByTagName("h4")[5].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function angus(){
+function angus() {
   let name = document.getElementsByTagName("h4")[6].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function violet(){
+function violet() {
   let name = document.getElementsByTagName("h4")[7].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function piper(){
+function piper() {
   let name = document.getElementsByTagName("h4")[8].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function maximus(){
+function maximus() {
   let name = document.getElementsByTagName("h4")[9].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function luna(){
+function luna() {
   let name = document.getElementsByTagName("h4")[10].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
 
-function stella(){
+function stella() {
   let name = document.getElementsByTagName("h4")[11].textContent;
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
@@ -88,21 +88,12 @@ function stella(){
 
 
 // JQUERY FUNCTIONS
-$('article').mouseenter(function() {
+// Puts a box shadow around the cards listing all the puppies.
+$('.container article').mouseenter(function () {
   $(this).fadeTo(300).css("box-shadow", "10px 10px #ECECEC");;
-}).mouseout(function(){
+}).mouseout(function () {
   $(this).fadeTo(300).css("box-shadow", "none");
 });
-
-
-
-
-// Console log the form data.
-$('#adoption-form').submit(function() {
-  var values = $(this).serialize();
-  console.log(values);
-});
-
 
 
 
@@ -110,7 +101,7 @@ $('#adoption-form').submit(function() {
 const articleContainer = document.getElementById("blog-posts-container");
 const postForm = document.getElementById("new-post-form");
 
-postForm.addEventListener("submit", function(event){
+postForm.addEventListener("submit", function (event) {
 
   event.preventDefault();
 
