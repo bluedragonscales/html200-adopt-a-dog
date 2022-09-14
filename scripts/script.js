f99c1f7c4234ac5e505797925c373ec41f3fa437
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-09-13 20:03:48 -0700

    Finished adding html notes and checkout total func

--- scripts/script.js
+++ scripts/script.js
@@ -1,100 +1,33 @@
 let checkoutTotal = document.getElementById("checkout-total");
 let cost = document.getElementById("cost");
 let total = 0;
-let formTotalCost = document.getElementById("total-cost");
+// let formTotalCost = document.getElementById("total-cost");
+let adoptedDogs = [];
 
-// Function to add total dollars to check out. Figure out how to truncate the decimals to 2 places.
-function addToCheckOutTotal() {
-  total = total + Number(cost.value);
-  checkoutTotal.textContent = "$" + total;
-  alert("Your total is now: $" + total);
+
+// Function to add total dollars to checkout. Figure out how to truncate the decimals to 2 places.
+function addToCheckOutTotal(dogName) {
+
+  if (!adoptedDogs.includes(dogName)) {
+    adoptedDogs.push(dogName);
+    total = total + Number(cost.textContent);
+    checkoutTotal.textContent = "$" + total;
+  } else {
+    alert("You've already selected this lucky puppy.");
+  }
+
 };
 
 
 
-
+// Function to alert the user of the dog's name when they click on a dog's card.
 function dogInfo(dogName) {
   alert(`This dog is named ${dogName}, and the adoption cost is $123.45.`);
 }
 
 
 
-// function murphy() {
-//   let name = document.getElementsByTagName("h4")[0].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
 
-// function poppy() {
-//   let name = document.getElementsByTagName("h4")[1].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function jack() {
-//   let name = document.getElementsByTagName("h4")[2].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function duffy() {
-//   let name = document.getElementsByTagName("h4")[3].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function lucas() {
-//   let name = document.getElementsByTagName("h4")[4].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function jake() {
-//   let name = document.getElementsByTagName("h4")[5].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function angus() {
-//   let name = document.getElementsByTagName("h4")[6].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function violet() {
-//   let name = document.getElementsByTagName("h4")[7].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function piper() {
-//   let name = document.getElementsByTagName("h4")[8].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function maximus() {
-//   let name = document.getElementsByTagName("h4")[9].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function luna() {
-//   let name = document.getElementsByTagName("h4")[10].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-// function stella() {
-//   let name = document.getElementsByTagName("h4")[11].textContent;
-//   let breed = "Good Dog";
-//   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
-// };
-
-
-
-
-// JQUERY FUNCTIONS
 // Puts a box shadow around the cards listing all the puppies.
 $('.container article').mouseenter(function () {
   $(this).fadeTo(300).css("box-shadow", "10px 10px #ECECEC");;
@@ -105,37 +38,37 @@ $('.container article').mouseenter(function () {
 
 
 // Form submit for new blog post
-const articleContainer = document.getElementById("blog-posts-container");
-const postForm = document.getElementById("new-post-form");
+// const articleContainer = document.getElementById("blog-posts-container");
+// const postForm = document.getElementById("new-post-form");
 
-postForm.addEventListener("submit", function (event) {
+// postForm.addEventListener("submit", function (event) {
 
-  event.preventDefault();
+//   event.preventDefault();
 
-  let blogTitle = document.getElementById("blog-title");
-  let post = document.getElementById("blog-words");
+//   let blogTitle = document.getElementById("blog-title");
+//   let post = document.getElementById("blog-words");
 
-  let articleImg = document.createElement("img");
-  articleImg.setAttribute('src', 'images/blog-2.jpg');
-  articleImg.setAttribute('class', 'blog-img');
-  let artDiv = document.createElement("div");
-  artDiv.setAttribute('class', 'blog-text');
-  let articleTitle = document.createElement("h3");
-  articleTitle.innerHTML = blogTitle.value;
-  console.log(articleTitle);
-  let articleContent = document.createElement("p");
-  articleContent.innerHTML = post.value;
-  console.log(articleContent);
+//   let articleImg = document.createElement("img");
+//   articleImg.setAttribute('src', 'images/blog-2.jpg');
+//   articleImg.setAttribute('class', 'blog-img');
+//   let artDiv = document.createElement("div");
+//   artDiv.setAttribute('class', 'blog-text');
+//   let articleTitle = document.createElement("h3");
+//   articleTitle.innerHTML = blogTitle.value;
+//   console.log(articleTitle);
+//   let articleContent = document.createElement("p");
+//   articleContent.innerHTML = post.value;
+//   console.log(articleContent);
 
-  let newArticle = document.createElement('article');
-  newArticle.appendChild(articleImg);
-  newArticle.appendChild(artDiv);
-  artDiv.appendChild(articleTitle);
-  artDiv.appendChild(articleContent);
-  articleContainer.insertBefore(newArticle, articleContainer.firstChild);
+//   let newArticle = document.createElement('article');
+//   newArticle.appendChild(articleImg);
+//   newArticle.appendChild(artDiv);
+//   artDiv.appendChild(articleTitle);
+//   artDiv.appendChild(articleContent);
+//   articleContainer.insertBefore(newArticle, articleContainer.firstChild);
 
-  alert("Thank you. The form information has been received.");
-  blogTitle.value = '';
-  post.value = '';
+//   alert("Thank you. The form information has been received.");
+//   blogTitle.value = '';
+//   post.value = '';
 
-});
+// });
