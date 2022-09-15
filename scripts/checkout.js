d13c10ecb9f3cf31fe632a799187be076ea55d90
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-09-14 21:47:14 -0700

    fixing checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,19 +2,23 @@ let checkoutTotal = document.getElementById("checkout-total");
 let cost = document.getElementById("cost");
 let total = 0;
 let adoptedDogs = [];
+let checkingOut = document.getElementById('total-cost');
 
 
 // Function to add total dollars to checkout. Figure out how to truncate the decimals to 2 places.
 function addToCheckOutTotal(dogName) {
 
     if (!adoptedDogs.includes(dogName)) {
-      adoptedDogs.push(dogName);
-      total = total + Number(cost.textContent);
-      localStorage.setItem('checkout-total', total);
-      console.log(localStorage.getItem('checkout-total'));
-      checkoutTotal.textContent = `$${localStorage.getItem('checkout-total')}`;
+        adoptedDogs.push(dogName);
+
+        total = total + Number(cost.textContent);
+        localStorage.setItem('checkout', total);
+
+        let finalTotal = Number(localStorage.getItem('checkout'));
+
+        checkoutTotal.textContent = `$${finalTotal}`;
+        checkingOut.textContent = `Total adoption cost: $${finalTotal}`;
     } else {
-      alert("You've already selected this lucky puppy.");
+        alert("You've already selected this lucky puppy.");
     }
-  
-  };
+};
